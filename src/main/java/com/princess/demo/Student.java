e8213989d83d036638452760098ba323583375ce
qingdeng_glb <703347383_glb@qq.com>
2020-12-15 15:26:23 +0800

    first commit

---
+++ src/main/java/com/princess/demo/Student.java
@@ -0,0 +1,19 @@
+package com.princess.demo;
+
+public class Student {
+	private int stuId;
+	private String name;
+	
+	public int getStuId() {
+		return stuId;
+	}
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/test/java/com/princess/test/TestDemo.java
@@ -0,0 +1,7 @@
+package com.princess.test;
+
+public class TestDemo {
+	public void test() {
+		System.out.println("打印测试语句");
+	}
+}
